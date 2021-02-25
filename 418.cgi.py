bfd3a530def5f39f6fab2203426ef7ef490f91ac
Yiu Man Ho <yiufamily.hh@gmail.com>
2021-02-25 12:48:40 +0800

    add 500, 418 utf8

--- 418.cgi.py
+++ 418.cgi.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 print("Status: 418 I'm a teapot")
-print("Content-Type: text/html")
+print("Content-Type: text/html; charset=utf-8")
 print()
 print(open("/var/www/html/main/418.html").read())
